33caa77adac1c6033a05e36ebc8f4c590f040895
ishita <ishitagops12@gmail.com>
2021-12-31 11:22:57 +0530

    converter program

--- Converter/convertercalc.java
+++ Converter/convertercalc.java
@@ -5,6 +5,21 @@ public class convertercalc
 {
     public static void calculate()
     {
-        
+        double inr, usd, euro, yen, meter, km, hr, sec, min;
+        int ch;
+        Scanner sc = new Scanner( System.in);
+        System.out.println("Enter your choice");
+        System.out.println("Enter your choice");
+        System.out.println("1. usd to inr \n2. euro to inr \n3. yen to inr \n4. meter to km \n5. hrs to min and sec");
+        ch = sc.nextInt();
+        switch(ch)
+        {
+            case 1 :
+                    System.out.in("USD TO INR");
+                    
+        }
+
+
+
     }
 }
