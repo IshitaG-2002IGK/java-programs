98e97b3c6ea4c3b1fa1a950a670c934297f87594
ishita <ishitagops12@gmail.com>
2021-12-31 16:07:53 +0530

    search and add functions added

--- stropr.java
+++ stropr.java
@@ -3,14 +3,34 @@ import java.util.ArrayList;
 
 public class stropr 
 {
-    private static ArrayList<String> var = new ArrayList <> ();
-    private static scan = new Scanner (System.in);
+    private static ArrayList< String > var = new ArrayList <> ();
+    private static Scanner sc = new Scanner (System.in);
     private static void append ()
     {
         System.out.println( "Enter the string to append");
-        
-        
+        String str = sc.next();
+        var.add(str);
         System.out.println( "Appended Successfully");
+
+    }
+    private static void add ()
+    {
+        System.out.println( "Enter the string to add");
+        String str = sc.next();
+        var.add(str);
+        System.out.println( "Enter the index of the string ");
+        int i = sc.nextInt();
+        var.add(i,str);
+        System.out.println( "Added Successfully!! ");
+
+    }
+
+    private static void search ()
+    {
+        System.out.println("Enter the strong to search for");
+        String str = sc.next();
+        int no =var.indexOf(str);
+        System.out.println("Found in Index postion " +no);
 
     }
 
