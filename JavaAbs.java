21ef9f419076bae1ab4364dcebdcdd0c21f4ece1
ishita <ishitagops12@gmail.com>
2021-12-31 17:02:50 +0530

    changes made

--- JavaAbs.java
+++ JavaAbs.java
@@ -36,7 +36,7 @@ public class JavaAbs
     public static void main (String args [])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println ("Enter the two numbers:")
+        System.out.println ("Enter the two numbers:");
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
         rectangle r = new rectangle();
