93b4e5895082f707758f0f396f6000a6f1cee1fe
ishita <ishitagops12@gmail.com>
2021-12-31 17:02:29 +0530

    area of shapes using abstract class implemented

--- JavaAbs.java
+++ JavaAbs.java
@@ -23,9 +23,31 @@ class triangle extends Shape
     public void printarea()
     {
         float area;
-        area = x*y;
+        area = 0.5*x*y;
         System.out.println ("Area of retangle is :" +area)
     }
 
 
 }
+
+
+public class JavaAbs
+{
+    public static void main (String args [])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Enter the two numbers:")
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        rectangle r = new rectangle();
+        r.x = x1 ;
+        r.y = y1 ;
+        r.printarea();
+        triangle t = new triangle();
+        t.x = x1;
+        t.y = y1 ;
+        t.printarea();
+    }
+
+}
+
