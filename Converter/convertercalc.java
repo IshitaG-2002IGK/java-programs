009996320a55a303aba83900cdb60fb472b16e3b
ishita <ishitagops12@gmail.com>
2021-12-31 11:57:00 +0530

    progress

--- Converter/convertercalc.java
+++ Converter/convertercalc.java
@@ -5,18 +5,47 @@ public class convertercalc
 {
     public static void calculate()
     {
-        double inr, usd, euro, yen, meter, km, hr, sec, min;
+        double inr, usd, meter, km, hr, sec, min;
         int ch;
         Scanner sc = new Scanner( System.in);
         System.out.println("Enter your choice");
         System.out.println("Enter your choice");
-        System.out.println("1. usd to inr \n2. euro to inr \n3. yen to inr \n4. meter to km \n5. hrs to min and sec");
+        System.out.println("1. usd to inr \n2. meter to km \n3. hrs to min and sec");
         ch = sc.nextInt();
         switch(ch)
         {
             case 1 :
-                    System.out.in("USD TO INR");
+                    System.out.println("USD TO INR");
+                    System.out.println("Enter the USD");
+                    usd = sc.nextDouble();
+                    inr =  usd * 60;
+                    System.out.println("The INR is :" + inr);
+                    System.out.println("INR to USD");
+                    System.out.println("Enter the INR");
+                    inr = sc.nextDouble();
+                    usd =  inr/60;
+                    System.out.println("The USD is :" + usd);
+                    break ;
+            
+            case 2 :
+
+                    System.out.println("Meters to km");
+                    System.out.println("Enter the meters");
+                    meter = sc.nextDouble();
+                    km =  meter / 1000;
+                    System.out.println("The km is :" + km);
+                    System.out.println("km to Meters");
+                    System.out.println("Enter the km");
+                    km = sc.nextDouble();
+                    meter =  km * 1000;
+                    System.out.println("The meter is :" + meter);
+
+            case 3 :
                     
+                    
+
+
+
         }
 
 
