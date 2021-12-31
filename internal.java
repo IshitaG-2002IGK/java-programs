38231b35e8e39d8244b9383107017d47800a648f
ishita <ishitagops12@gmail.com>
2021-12-31 23:53:03 +0530

    internal marks calculation

---
+++ internal.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class internal
+{
+public static void main(String[] args) {
+
+Scanner in=new Scanner(System.in);
+String name;
+int days,num,work;
+System.out.println("Enter the name of the student: ");
+name=in.next();
+System.out.println("Enter the Register No of the student: ");
+num=in.nextInt();
+System.out.println("Enter the total number of working days: ");
+work=in.nextInt();
+System.out.println("Enter total number of according days present: ");
+days=in.nextInt();
+double attendance_perc=(work*days)/7.0;
+int marks=0;
+if (attendance_perc>=90){
+marks=5;
+}
+else if(attendance_perc>=80){
+marks=4;
+}
+else if(attendance_perc >=70) {
+marks=3;
+}
+System.out.println("Name: "+name);
+System.out.println("Register Number:"+num);
+System.out.println("Number of days: "+days);
+System.out.println("Marks: "+marks);
+
+}
+}
+
